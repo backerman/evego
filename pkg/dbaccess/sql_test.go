7924dc9ac18b787b007a65e747b1dbb99bfb2421
Brad Ackerman <brad@facefault.org>
2015-01-22 23:18:50 -0500

    Update copyright year.

--- pkg/dbaccess/sql_test.go
+++ pkg/dbaccess/sql_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2014 Brad Ackerman.
+Copyright © 2014–5 Brad Ackerman.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
